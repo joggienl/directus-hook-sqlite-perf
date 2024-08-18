7dab4852a37ce6d4152a3bbdb657528460ed208a
Jogchum Koerts <jogchum@clevercactus.dev>
2024-08-18 16:02:01 +0200

    refactor: getPragmasFromEnv

--- src/index.js
+++ src/index.js
@@ -25,113 +25,99 @@ function checkKnexConfig(knex, logger) {
  * Returns an array with statements to execute.
  *
  * @param env
- * @returns {*[]}
+ * @returns {String[]}
  */
 function getPragmasFromEnv(env, logger) {
 	const pragmas = []
 
-	const pragmaValues = {
+	const PRAGMA_VALUES = {
 		'journal_mode': ['delete', 'truncate', 'persist', 'memory', 'wal', 'off'],
 		'synchronous': ['off', 'normal', 'full', 'extra'],
 		'temp_store': ['default', 'file', 'memory'],
 	}
 
-	if (env.DHSP_BUSY_TIMEOUT) {
-		if (isNumeric(env.DHSP_BUSY_TIMEOUT)) {
-			pragmas.push(`PRAGMA busy_timeout = ${env.DHSP_BUSY_TIMEOUT};`)
-		} else {
-			logger.error('Please use a numeric value for DHSP_BUSY_TIMEOUT')
-			pragmas.push(`PRAGMA busy_timeout = 30000;`)
-		}
-	} else {
-		// Set busy_timeout to prevent (some) SQLITE_BUSY errors from happening.
-		// Set it to 30 seconds by default.
-		pragmas.push(`PRAGMA busy_timeout = 30000;`)
+	const PRAGMA_DEFAULT_VALUES = {
+		busy_timeout: 30000,
+		journal_mode: 'wal',
+		journal_size: 5242880,
+		cache_size: -20000,
+		synchronous: 'normal',
+		temp_store: 'memory',
+		mmap_size: 512000000,
+		page_size: undefined // No default
 	}
 
-	if (env.DHSP_JOURNAL_MODE &&
-		pragmaValues.journal_mode.includes(env.DHSP_JOURNAL_MODE.toLowerCase())) {
-		pragmas.push(`PRAGMA journal_mode = ${env.DHSP_JOURNAL_MODE};`)
-	} else {
-		// This extension forces WAL mode as default.
-		pragmas.push(`PRAGMA journal_mode = wal;`)
-	}
-
-	if (env.DHSP_JOURNAL_SIZE) {
-		if (isNumeric(env.DHSP_JOURNAL_SIZE)) {
-			pragmas.push(`PRAGMA journal_size = ${env.DHSP_JOURNAL_SIZE};`)
-		} else {
-			logger.error('Please use a numeric value for DHSP_JOURNAL_SIZE')
-			// 5MB default for max wal file size
-			pragmas.push(`PRAGMA journal_size = 5242880;`)
-		}
-	} else {
-		// 5MB default for max wal file size
-		pragmas.push(`PRAGMA journal_size = 5242880;`)
-	}
-
-	if (env.DHSP_CACHE_SIZE) {
-		if (isNumeric(env.DHSP_CACHE_SIZE)) {
-			pragmas.push(`PRAGMA cache_size = ${env.DHSP_CACHE_SIZE};`)
-		} else {
-			logger.error('Please use a numeric value for DHSP_CACHE_SIZE')
-			// set journal_size to negative number to improve read speeds
-			pragmas.push(`PRAGMA cache_size = -20000;`)
-		}
-	} else {
-		// set journal_size to negative number to improve read speeds
-		pragmas.push(`PRAGMA cache_size = -20000;`)
-	}
-
-	if (env.DHSP_SYNCHRONOUS &&
-		pragmaValues.synchronous.includes(env.DHSP_SYNCHRONOUS.toLowerCase())) {
-		pragmas.push(`PRAGMA synchronous = ${env.DHSP_SYNCHRONOUS};`)
-	} else {
-		// This extension sets synchronous to normal by default
-		pragmas.push(`PRAGMA synchronous = normal;`)
-	}
-
-	if (env.DHSP_TEMP_STORE &&
-		pragmaValues.temp_store.includes(env.DHSP_TEMP_STORE.toLowerCase())) {
-		pragmas.push(`PRAGMA temp_store = ${env.DHSP_TEMP_STORE};`)
-	} else {
-		// This extension sets memory as default location for temporary things
-		pragmas.push(`PRAGMA temp_store = memory;`)
-	}
-
-	if (env.DHSP_MMAP_SIZE) {
-		if (isNumeric(env.DHSP_MMAP_SIZE)) {
-			pragmas.push(`PRAGMA mmap_size = ${env.DHSP_MMAP_SIZE};`)
-		} else {
-			logger.error('Please use a numeric value for DHSP_MMAP_SIZE')
-			// This extension will set mmap_size to 512MB by default
-			pragmas.push(`PRAGMA mmap_size = 512000000;`)
-		}
-	} else {
-		// This extension will set mmap_size to 512MB by default
-		pragmas.push(`PRAGMA mmap_size = 512000000;`)
-	}
-
-	if (env.DHSP_PAGE_SIZE) {
-		if (isNumeric(env.DHSP_PAGE_SIZE)) {
-			pragmas.push(`PRAGMA page_size = ${env.DHSP_PAGE_SIZE};`)
-		} else {
-			logger.error('Please use a numeric value for DHSP_PAGE_SIZE')
-			// We do not set a default value for page_size!
+	const pushPragma = (key, value, numeric, defaultValue) => {
+		if (value) {
+			if (numeric && !isNumeric(value)) {
+				logger.error(`Please use a numeric value for ${key.toUpperCase()}`)
+				value = defaultValue
+			}
+			pragmas.push(`PRAGMA ${key} = ${value};`)
+		} else if (defaultValue !== undefined) {
+			pragmas.push(`PRAGMA ${key} = ${defaultValue};`)
 		}
 	}
+
+	pushPragma('busy_timeout',
+		env.DHSP_BUSY_TIMEOUT,
+		true,
+		DEFAULT_VALUES.busy_timeout
+	)
+
+	pushPragma('journal_mode',
+	env.DHSP_JOURNAL_MODE && PRAGMA_VALUES.journal_mode.includes(env.DHSP_JOURNAL_MODE.toLowerCase())
+		? env.DHSP_JOURNAL_MODE
+		: DEFAULT_VALUES.journal_mode
+	)
+
+	pushPragma('journal_size',
+		env.DHSP_JOURNAL_SIZE,
+		true,
+		DEFAULT_VALUES.journal_size
+	)
+
+	pushPragma('cache_size',
+		env.DHSP_CACHE_SIZE,
+		true,
+		DEFAULT_VALUES.cache_size
+	)
+
+	pushPragma('synchronous',
+	env.DHSP_SYNCHRONOUS && PRAGMA_VALUES.synchronous.includes(env.DHSP_SYNCHRONOUS.toLowerCase())
+		? env.DHSP_SYNCHRONOUS
+		: DEFAULT_VALUES.synchronous
+	)
+
+	pushPragma('temp_store',
+	env.DHSP_TEMP_STORE && PRAGMA_VALUES.temp_store.includes(env.DHSP_TEMP_STORE.toLowerCase())
+		? env.DHSP_TEMP_STORE
+		: DEFAULT_VALUES.temp_store
+	)
+
+	pushPragma('mmap_size',
+		env.DHSP_MMAP_SIZE,
+		true,
+		DEFAULT_VALUES.mmap_size
+	)
+
+	pushPragma('page_size',
+		env.DHSP_PAGE_SIZE,
+		true,
+		DEFAULT_VALUES.page_size
+	)
 
 	return pragmas
 }
 
 export default async (_, {database, logger, env}) => {
-	// Skip we are not using sqlite3;
+	// Skip we are not using sqlite3.
 	if (database.client.config.client !== 'sqlite3') return
 
 	// Check the current configuration
 	checkKnexConfig(database, logger)
 
-	// Get values or set defaults for the settings we want to set;
+	// Get values or set defaults for the settings we want to set.
 	const pragmas = getPragmasFromEnv(env, logger)
 
 	logger.debug(pragmas)
@@ -151,9 +137,9 @@ export default async (_, {database, logger, env}) => {
 		await Promise.all(pragmas.map((pragma) => {
 			return new Promise((resolve, reject) => {
 				conn.run(pragma, (error) => {
-					if (error) reject(error);
-					else resolve();
-				});
+					if (error) reject(error)
+					else resolve()
+				})
 			})
 		}))
 
